3a4bed3c3875f5b9ef6c141ac5601df9102cc732
Bob Yexley <bob@yexley.net>
2018-12-13 09:29:34 -0500

    Added Poplar to Open Source section, and upgraded a bunch of dependencies

--- src/build/tasks/css.js
+++ src/build/tasks/css.js
@@ -1,17 +1,20 @@
 var path = require("path");
 var gulp = require("gulp");
 var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer-core");
-var cssnext = require("cssnext");
-var imports = require("postcss-import");
+// var autoprefixer = require("autoprefixer");
+var cssnext = require("postcss-cssnext");
+var imports = require("postcss-easy-import");
 var nested = require("postcss-nested");
 var variables = require("postcss-simple-vars");
 var mixins = require("postcss-mixins");
 var csswring = require("csswring");
 
+const CSS_SRC = path.resolve("src/css");
+const ASSETS_DIR = path.resolve("assets/css");
+
 var processors = [
-  autoprefixer({ browsers: ["last 2 version"]}),
-  imports({ path: path.join(__dirname, "../../assets/css"), glob: true }),
+  // autoprefixer({ browsers: ["last 2 version"]}),
+  imports({ path: CSS_SRC, glob: true }),
   cssnext(),
   mixins,
   nested,
@@ -21,8 +24,8 @@ var processors = [
 
 gulp.task("css", ["clean:css"], function () {
 
-  gulp.src(path.join(__dirname, "../../css", "main.css"))
+  gulp.src(path.join(CSS_SRC, "main.css"))
       .pipe(postcss(processors))
-      .pipe(gulp.dest(path.join(__dirname, "../../../assets/css")));
+      .pipe(gulp.dest(ASSETS_DIR));
 
 });
